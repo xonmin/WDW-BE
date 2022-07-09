0fe356652c92c87fbbabea33d6deefb2f687ceef
KangSungBum <peter9706@naver.com>
2022-07-09 16:31:46 +0900

    Initial Commit

---
+++ src/main/java/com/wdw/wdw/WdwApplication.java
@@ -0,0 +1,13 @@
+package com.wdw.wdw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WdwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wdw/wdw/WdwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wdw.wdw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WdwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
