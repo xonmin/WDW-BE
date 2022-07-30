3772b0421022ace347f185ef4463640f29da327b
road3144 <fnfnfn258@gmail.com>
2022-07-30 17:07:44 +0900

    feat: user attributes 추가, getRoleList function 추가

--- src/main/java/com/wdw/wdw/domain/User.java
+++ src/main/java/com/wdw/wdw/domain/User.java
@@ -1,22 +1,55 @@
 package com.wdw.wdw.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Entity
-@Getter @Setter
+@Data
+@NoArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue
     @Column(name = "user_seq")
     private Long id;
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+    private String roles; // USER, ADMIN
+    private String provider;
+    private String providerId;
+    @CreationTimestamp
+    private Timestamp createDate;
 
+    public List<String> getRoleList() {
+        if (this.roles.length() > 0) {
+            return Arrays.asList(this.roles.split(","));
+        }
+        return new ArrayList<>();
+    }
+
+    @Builder
+    public User(Long id, String username, String password, String name, String email, String roles, String provider, String providerId, Timestamp createDate) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.roles = roles;
+        this.provider = provider;
+        this.providerId = providerId;
+        this.createDate = createDate;
+    }
 
 }
