cb5ec0b08c37fe88224ff73b1940511c55fb0c19
road3144 <fnfnfn258@gmail.com>
2022-07-31 14:26:53 +0900

    feat: 로그인, 회원가입 및 권한 검사 기능 추가

--- src/main/java/com/wdw/wdw/WdwApplication.java
+++ src/main/java/com/wdw/wdw/WdwApplication.java
@@ -1,11 +1,19 @@
 package com.wdw.wdw;
 
+import com.wdw.wdw.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class WdwApplication {
-
+    @Bean
+    public BCryptPasswordEncoder encodePassword(){
+        return new BCryptPasswordEncoder();
+    }
     public static void main(String[] args) {
         SpringApplication.run(WdwApplication.class, args);
         //git branch test
