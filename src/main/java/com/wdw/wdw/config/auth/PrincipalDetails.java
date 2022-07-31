d32cc8f4fa86691e3cf0ad38ff0b7c8de2d0f60e
road3144 <fnfnfn258@gmail.com>
2022-07-31 14:26:53 +0900

    feat: 로그인, 회원가입 및 권한 검사 기능 추가

---
+++ src/main/java/com/wdw/wdw/config/auth/PrincipalDetails.java
@@ -0,0 +1,76 @@
+package com.wdw.wdw.config.auth;
+
+import com.wdw.wdw.domain.User;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+@Data
+public class PrincipalDetails implements OAuth2User, UserDetails {
+
+    private User user;
+
+    private Map<String, Object> attributes;
+
+    public PrincipalDetails(User user) {
+        this.user = user;
+    }
+
+    public PrincipalDetails(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        user.getRoleList().forEach(r -> authorities.add(() -> r));
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    //휴먼회원 관리
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+}
---
+++ src/main/java/com/wdw/wdw/config/jwt/JwtTokenProvider.java
@@ -0,0 +1,44 @@
+package com.wdw.wdw.config.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.wdw.wdw.config.AppProperties;
+import com.wdw.wdw.config.auth.PrincipalDetails;
+import lombok.Data;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtTokenProvider {
+
+    private AppProperties appProperties;
+
+    public JwtTokenProvider(AppProperties appProperties) {
+        this.appProperties = appProperties;
+    }
+
+    public String createToken(Authentication authentication) {
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+        Date expiredDate = new Date(System.currentTimeMillis() + appProperties.getAuth().getTokenExpirationMsec());
+
+        return JWT.create()
+                .withSubject("wdw")
+                .withExpiresAt(expiredDate)
+                .withClaim("id", principalDetails.getUser().getId())
+                .withClaim("username", principalDetails.getUser().getUsername())
+                .sign(Algorithm.HMAC512(appProperties.getAuth().getTokenSecret()));
+    }
+    public Long getUserIdFromToken(String token) {
+        DecodedJWT jwt = JWT.decode(token);
+        return jwt.getClaim("id").asLong();
+    }
+
+    public String getUsernameFromToken(String token) {
+        DecodedJWT jwt = JWT.decode(token);
+        return jwt.getClaim("username").asString();
+    }
+
+}
