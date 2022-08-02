9f8428107cf064693285bda6870b888da9773ffa
KangSungBum <peter9706@naver.com>
2022-08-02 21:37:53 +0900

    refactor: rename class(Log->Record)

--- src/main/java/com/wdw/wdw/repository/RecordRepository.java
+++ src/main/java/com/wdw/wdw/repository/RecordRepository.java
@@ -1,6 +1,6 @@
 package com.wdw.wdw.repository;
 
-import com.wdw.wdw.domain.Log;
+import com.wdw.wdw.domain.Record;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -10,21 +10,21 @@ import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
-public class LogRepository {
+public class RecordRepository {
 
     @PersistenceContext
     private final EntityManager em;
 
-    public void save(Log log) {
-        em.persist(log);
+    public void save(Record record) {
+        em.persist(record);
     }
 
-    public Log findOne(Long id) {
-        return em.find(Log.class, id);
+    public Record findOne(Long id) {
+        return em.find(Record.class, id);
     }
 
-    public List<Log> findAll(String name) {
-        return em.createQuery("select l from Log l where l.user = :user", Log.class)
+    public List<Record> findAll(String name) {
+        return em.createQuery("select r from Record r where r.user = :user", Record.class)
                 .setParameter("user", name)
                 .getResultList();
     }
