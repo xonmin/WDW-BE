796b4f181f2fdbdd9c8ed664894a61a368d00385
KangSungBum <peter9706@naver.com>
2022-08-02 16:45:31 +0900

    feat: Log Repository 추가

--- src/main/java/com/wdw/wdw/repository/LogRepository.java
+++ src/main/java/com/wdw/wdw/repository/LogRepository.java
@@ -1,15 +1,32 @@
 package com.wdw.wdw.repository;
 
+import com.wdw.wdw.domain.Log;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.Entity;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class LogRepository {
 
+    @PersistenceContext
     private final EntityManager em;
+
+    public void save(Log log) {
+        em.persist(log);
+    }
+
+    public Log findOne(Long id) {
+        return em.find(Log.class, id);
+    }
+
+    public List<Log> findAll(String name) {
+        return em.createQuery("select l from Log l where l.user = :user", Log.class)
+                .setParameter("user", name)
+                .getResultList();
+    }
 
 }
