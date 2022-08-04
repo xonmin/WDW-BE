c2a0121eba503966f46a94be79e709d77165403e
KangSungBum <peter9706@naver.com>
2022-08-04 14:16:12 +0900

    refactor: Domain명 변경

--- src/main/java/com/wdw/wdw/repository/RecordRepository.java
+++ src/main/java/com/wdw/wdw/repository/RecordRepository.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -27,6 +30,23 @@ public class RecordRepository {
         return em.createQuery("select r from Record r where r.user = :user", Record.class)
                 .setParameter("user", name)
                 .getResultList();
+    }
+
+    public List<Record> findRecordsByDay(LocalDateTime date) {
+        //오늘을 기준으로 조회하는 경우면 날짜로만 처리해야한다.
+        return em.createQuery("select r from Record r where r.recordDate.year = :year and r.recordDate.month = :month and r.recordDate.day = :day order by r.recordDate.time", Record.class)
+                .setParameter("year", date.getYear())
+                .setParameter("month", date.getMonthValue())
+                .setParameter("day", date.getDayOfMonth())
+                .getResultList();
+    }
+
+    public List<Record> findRecordsByWeek(LocalDateTime date) {
+        return new ArrayList<>();
+    }
+
+    public List<Record> findRecordsByMonth(LocalDateTime date) {
+        return new ArrayList<>();
     }
 
 }
