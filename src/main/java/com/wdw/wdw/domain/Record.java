09e59e8b0abd7401dbb8b0abe78b68254f8dc4a4
KangSungBum <peter9706@naver.com>
2022-08-04 13:59:29 +0900

    feat: Record_Domain 필드 추가

--- src/main/java/com/wdw/wdw/domain/Record.java
+++ src/main/java/com/wdw/wdw/domain/Record.java
@@ -1,13 +1,16 @@
 package com.wdw.wdw.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor
 public class Record {
 
     @Id
@@ -16,8 +19,20 @@ public class Record {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_seq")
     private User user;
 
-    private LocalDateTime date;
+    @Embedded
+    private Date date;
     private int quantity;
+
+    public Record(int quantity) {
+        this.quantity = quantity;
+        this.date = new Date(LocalDate.now().getYear(), LocalDate.now().getMonthValue(), LocalDate.now().getDayOfMonth(), LocalDateTime.now());
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+        user.getRecords().add(this);
+    }
 }
