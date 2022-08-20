56a36220fc8fec0f4914de6c2fd06faaa597301f
KangSungBum <peter9706@naver.com>
2022-08-20 15:05:40 +0900

    feat: BadgeType enum 추가

---
+++ src/main/java/com/wdw/wdw/domain/BadgeType.java
@@ -0,0 +1,10 @@
+package com.wdw.wdw.domain;
+
+public enum BadgeType {
+    SUM_BRONZE, //10L
+    SUM_SILVER, //50L
+    SUM_GOLD,   //100L
+    CON_BRONZE, //1week
+    CON_SILVER, //1month
+    CON_GOLD,   //6month
+}
