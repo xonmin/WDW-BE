85e66e3a6541bfd4d3ab6ac4e6724c40e00122cd
KangSungBum <peter9706@naver.com>
2022-08-20 15:05:22 +0900

    feat: Badge domain 추가

---
+++ src/main/java/com/wdw/wdw/domain/Badge.java
@@ -0,0 +1,26 @@
+package com.wdw.wdw.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+public class Badge {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "badge_seq")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private BadgeType badgeType;
+
+    @OneToMany(mappedBy = "badge")
+    private List<Achievement> achievements = new ArrayList<>();
+
+    private String badgeName;
+    private String badgeImage;
+}
