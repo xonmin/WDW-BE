91ed81dff4dcebcbd38c4f1d93c694cd5753a098
KangSungBum <peter9706@naver.com>
2022-08-24 14:56:21 +0900

    feat: BASIC badge 추가

--- src/main/java/com/wdw/wdw/domain/BadgeType.java
+++ src/main/java/com/wdw/wdw/domain/BadgeType.java
@@ -1,9 +1,11 @@
 package com.wdw.wdw.domain;
 
 public enum BadgeType {
+    SUM_BASIC,
     SUM_BRONZE, //10L
     SUM_SILVER, //50L
     SUM_GOLD,   //100L
+    CON_BASIC,
     CON_BRONZE, //1week
     CON_SILVER, //1month
     CON_GOLD,   //6month
