07d7e9f0ac72eb941698ff7e4678636c34da3606
KangSungBum <peter9706@naver.com>
2022-09-06 17:30:26 +0900

    feat: weekly, monthly 조회 메소드 완성

--- src/main/java/com/wdw/wdw/service/RecordService.java
+++ src/main/java/com/wdw/wdw/service/RecordService.java
@@ -2,10 +2,8 @@ package com.wdw.wdw.service;
 
 import com.wdw.wdw.domain.Record;
 import com.wdw.wdw.domain.User;
-import com.wdw.wdw.dto.record.RecordAddRequestDto;
-import com.wdw.wdw.dto.record.RecordAddResponseDto;
-import com.wdw.wdw.dto.record.RecordGetResponseListDto;
-import com.wdw.wdw.dto.record.RecordGetResponseValueDto;
+import com.wdw.wdw.dto.record.*;
+import com.wdw.wdw.infra.util.CalendarUtil;
 import com.wdw.wdw.repository.RecordRepository;
 import com.wdw.wdw.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,8 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.Calendar;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -43,39 +40,51 @@ public class RecordService {
         return RecordAddResponseDto.from(user, record);
     }
 
-    public RecordGetResponseValueDto findDailyRecord(User user, LocalDate date) {
-        Integer rtnValue = recordRepository.findByDailyDate(user, date);
-        return RecordGetResponseValueDto.from(rtnValue);
+    public RecordGetDailyWaterResponseDto findDailyRecord(User user, LocalDate date) {
+        Integer dailyIntake = recordRepository.findDailyRecords(user, date);
+        if (dailyIntake == null) dailyIntake = 0;
+        return RecordGetDailyWaterResponseDto.from(date, dailyIntake);
     }
 
 
-    public RecordGetResponseListDto findWeeklyRecord(User user, LocalDate date) {
-        Calendar cal = Calendar.getInstance();
-        int week;
-        cal.set(date.getYear(), date.getMonthValue() - 1, date.getDayOfMonth());
+    public RecordGetWaterListResponseDto findWeeklyRecord(User user, LocalDate date) {
+        LocalDate startDay = CalendarUtil.getStartDayOfWeek(date);
+        LocalDate endDay = CalendarUtil.getEndDayOfWeek(date);
 
-        Calendar sday = (Calendar) cal.clone();
-        week = sday.get(Calendar.DAY_OF_WEEK);
-        sday.add(Calendar.DATE, -(week - 1));
-        LocalDate startDay = LocalDate.ofInstant(sday.toInstant(), ZoneId.systemDefault());
+        List<Object[]> dayRecord = recordRepository.findPastRecords(user, startDay, endDay);
 
-        Calendar eday = (Calendar) cal.clone();
-        week = eday.get(Calendar.DAY_OF_WEEK);
-        eday.add(Calendar.DATE, 7 - week);
-        LocalDate endDay = LocalDate.ofInstant(sday.toInstant(), ZoneId.systemDefault());
+        List<CustomizedCalendarDto> weeklyRecords = convertIntoDtoList(dayRecord);
 
-        List<Record> rtnList = recordRepository.findByWeeklyDate(user, startDay, endDay);
-
-        return RecordGetResponseListDto.from(rtnList);
+        return RecordGetWaterListResponseDto.from(weeklyRecords);
     }
 
-    public RecordGetResponseListDto findMonthlyRecord(User user, LocalDate date) {
+    public RecordGetWaterListResponseDto findMonthlyRecord(User user, LocalDate date) {
         int curMonth = date.getMonthValue();
         LocalDate startDay = LocalDate.of(date.getYear(), curMonth, 1);
 
-        List<Record> rtnList = recordRepository.findByMonthlyDate(user, startDay, date);
+        List<Object[]> monthRecord = recordRepository.findPastRecords(user, startDay, LocalDate.now());
 
-        return RecordGetResponseListDto.from(rtnList);
+        List<CustomizedCalendarDto> monthlyRecords = convertIntoDtoList(monthRecord);
+
+        return RecordGetWaterListResponseDto.from(monthlyRecords);
+    }
+
+    public List<CustomizedCalendarDto> convertIntoDtoList(List<Object[]> targetList) {
+        List<CustomizedCalendarDto> convertedList = new ArrayList<>();
+
+        for (Object[] target : targetList) {
+            LocalDate recordDate = (LocalDate) target[0];
+            Long dailyIntake = (Long) target[1];
+
+            CustomizedCalendarDto customizedDto = CustomizedCalendarDto.builder()
+                    .recordTime(recordDate)
+                    .totalSum(dailyIntake)
+                    .build();
+
+            convertedList.add(customizedDto);
+        }
+
+        return convertedList;
     }
 
     public boolean isEnough(User user, LocalDate dateTime){
