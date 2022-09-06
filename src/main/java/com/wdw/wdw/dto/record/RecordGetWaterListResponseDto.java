e4eff5d5453329ea81fdbe2cfe93aab221526d59
KangSungBum <peter9706@naver.com>
2022-09-06 17:29:17 +0900

    feat: 반환타입에 따른 ResponseDto 분리

---
+++ src/main/java/com/wdw/wdw/dto/record/RecordGetWaterListResponseDto.java
@@ -0,0 +1,25 @@
+package com.wdw.wdw.dto.record;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class RecordGetWaterListResponseDto {
+
+    List<CustomizedCalendarDto> records;
+
+    @Builder
+    public RecordGetWaterListResponseDto(List<CustomizedCalendarDto> records) {
+        this.records = records;
+    }
+
+    public static RecordGetWaterListResponseDto from(List<CustomizedCalendarDto> records) {
+        return RecordGetWaterListResponseDto.builder()
+                .records(records)
+                .build();
+    }
+}
