5658fc4818c145cfcc181480a0051522b65ab65b
han51361 <han51361@gmail.com>
2022-09-09 11:49:51 +0900

    feature : update badgeType

--- src/main/java/com/wdw/wdw/domain/BadgeType.java
+++ src/main/java/com/wdw/wdw/domain/BadgeType.java
@@ -1,12 +1,21 @@
 package com.wdw.wdw.domain;
 
+import lombok.Getter;
+
+@Getter
 public enum BadgeType {
-    SUM_BASIC,
-    SUM_BRONZE, //10L
-    SUM_SILVER, //50L
-    SUM_GOLD,   //100L
-    CON_BASIC,
-    CON_BRONZE, //1week
-    CON_SILVER, //1month
-    CON_GOLD,   //6month
+    SUM_BASIC(0, null),
+    SUM_BRONZE(10000, null), // 10L
+    SUM_SILVER(50000, null), // 50L
+    SUM_GOLD(100000, null),   // 100L
+    CON_BASIC(null, 0),
+    CON_BRONZE(null, 7), // 1week
+    CON_SILVER(null, 30), // 1month
+    CON_GOLD(null, 180);   // 6month
+
+    private Integer intakeOfSum;
+    private Integer consecutiveDay;
+
+    BadgeType(Integer intakeOfSum, Integer consecutiveDay) {
+    }
 }
